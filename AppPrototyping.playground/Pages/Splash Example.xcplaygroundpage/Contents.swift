d6f275d0ea755bbfffcdd661686378109e5cc1f2
Timothy Bellay <timbellay@Timothys-Air.home>
2015-12-25 22:21:01 -0500

    fix statusBar.

--- AppPrototyping.playground/Pages/Splash Example.xcplaygroundpage/Contents.swift
+++ AppPrototyping.playground/Pages/Splash Example.xcplaygroundpage/Contents.swift
@@ -6,5 +6,6 @@ import XCPlayground
 let device = Device.iPhone6SPlus
 let screen = Screen(device: device, type: .splash, imageName: "splash")
 XCPlayground.XCPlaygroundPage.currentPage.liveView = screen.view
+//outlineViews([screen.view], outlineColor: .whiteColor())
 
 //: [Next](@next)
--- AppPrototyping.playground/Pages/StatusBar.xcplaygroundpage/Contents.swift
+++ AppPrototyping.playground/Pages/StatusBar.xcplaygroundpage/Contents.swift
@@ -16,12 +16,11 @@ import XCPlayground
 // TODO: add wifi/no signal label and strength meter to carrierSV
 // TODO: set time with a string
 
+let device = Device.iPhone6Plus
+let frame = device.frame()
 
-let frame = CGRectMake(0, 0, 375, 20)
-var statusBar = StatusBar(frame: frame, theme: .light
-)
-
+let statusBar = StatusBar(frame: frame, theme: .light)
+XCPlayground.XCPlaygroundPage.currentPage.liveView = statusBar.view
 //outlineViews([statusBar.view], outlineColor: .blackColor())
 
-XCPlayground.XCPlaygroundPage.currentPage.liveView = statusBar.view
 //: [Next](@next)
