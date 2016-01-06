714259eff7c44b67a45ee0bcd1cc3b655903dd3c
Tim Bellay <tim@metaneer.com>
2016-01-06 12:08:21 -0800

    Scrollers react to scrolling.

--- AppPrototyping.playground/Pages/ScrollView.xcplaygroundpage/Contents.swift
+++ AppPrototyping.playground/Pages/ScrollView.xcplaygroundpage/Contents.swift
@@ -3,7 +3,7 @@
 import UIKit
 import XCPlayground
 
-let device = Device.iPhone6
+let device = Device.iPhone6Plus
 let frame = device.frame()
 let view = UIView(frame: frame)
 let sv = makeVerticalSV(view)
@@ -11,10 +11,10 @@ let statusBar = StatusBar(frame: frame, theme: .light)
 sv.addArrangedSubview(statusBar.view)
 horizontalStrechToParentView(statusBar.view)
 
-let svc = ScrollView(device: device, imageName: "splash", scrollDirection: .Both, width: nil, height: 500)
+let svc = ScrollView(device: device, imageName: "splash", scrollDirection: .Both, width: nil, height: 500, showScrollers: true)
 sv.addArrangedSubview(svc.view)
 svc.center()
-//svc.scroll(.Left, amount: 100)
+svc.scroll(.Left, amount: 100)
 
 let tabbar = TabBar(frame: frame, theme: .light)
 sv.addArrangedSubview(tabbar.view)
--- AppPrototyping.playground/Sources/Scrolls.swift
+++ AppPrototyping.playground/Sources/Scrolls.swift
@@ -10,6 +10,7 @@
 import UIKit
 
 public enum Direction {
+	// NB: Scroll .Up means that the content moves up and that the user has swipped up.
 	case Up, Down, Left, Right
 }
 
@@ -18,53 +19,142 @@ public enum ScrollDirection {
 }
 
 public class ScrollView: NSObject, UIScrollViewDelegate {
-	public let view = UIScrollView(frame: .zero)
+	// scrollView properties
 	let device: Device?
+	public let view = UIScrollView(frame: .zero)
+	var viewWidthAnchor: CGFloat?
+	var viewHeightAnchor: CGFloat?
+	var contentHeight: CGFloat?
+	var contentWidth: CGFloat?
+	
+	// scroller properties
 	var scrollDirection: ScrollDirection = .Both
-	var showHorizontalScroller = true
+	var showScrollers = true
 	var showVerticalScroller = true
-	public init(device: Device, imageName: String?, scrollDirection: ScrollDirection, width: CGFloat?, height: CGFloat?) {
+	var showHorizontalScroller = true
+	var verticalScroller: UIView?
+	var horizontalScroller: UIView?
+	let scrollerOffset = 6 as CGFloat
+	let scrollerThickness = 6 as CGFloat
+
+	
+	public init(device: Device, imageName: String?, scrollDirection: ScrollDirection, width: CGFloat?, height: CGFloat?, showScrollers: Bool) {
 		self.device = device
 		self.scrollDirection = scrollDirection
+		self.showScrollers = showScrollers
+		
+		// Setup view anchors.
+		if let widthAnchor = width {
+			view.widthAnchor.constraintEqualToConstant(widthAnchor).active = true
+			self.viewWidthAnchor = widthAnchor
+		} else {
+			view.widthAnchor.constraintEqualToConstant(device.frame().width).active = true
+			self.viewWidthAnchor = device.frame().width
+		}
+		
+		if let heightAnchor = height {
+			view.heightAnchor.constraintEqualToConstant(heightAnchor).active = true
+			self.viewHeightAnchor = heightAnchor
+		} else {
+			view.heightAnchor.constraintEqualToConstant(44).active = true
+			self.viewHeightAnchor = 44
+			print("WARNING: set scrollview height to minimum height: 44")
+		}
+		
+		// Setup content and content size.
 		if let name = imageName {
 			if let image = UIImage(named: name) {
 				let imageView = UIImageView(image: image)
 				view.addSubview(imageView)
 				view.contentSize = imageView.frame.size
+				contentHeight = imageView.frame.size.height
+				contentWidth = imageView.frame.size.width
 			}
+		} else {
+			contentWidth = viewWidthAnchor
+			contentHeight = viewHeightAnchor
 		}
+		view.contentSize = CGSizeMake(contentWidth!, contentHeight!) // TODO: if more views are added, contentSize needs to be updated to show all scrollable area and proper scroller bars. TB.
 		
+		super.init()
+		view.delegate = self
+		
+		setupScrollers() // Warning: Only call setupScrollers() here, after content size and view anchors have been set.
+
 		switch scrollDirection {
 		case .None:
 			view.scrollEnabled = false
 			showHorizontalScroller = false
 			showVerticalScroller = false
+			self.showScrollers = false
+			verticalScroller = nil
+			horizontalScroller = nil
 		case .Both:
 			break
 		case .Vertical:
 			showHorizontalScroller = false
+			horizontalScroller = nil
 		case .Horizontal:
 			showVerticalScroller = false
-		}
-		
-		if let widthAnchor = width {
-			view.widthAnchor.constraintEqualToConstant(widthAnchor).active = true
-		} else {
-			view.widthAnchor.constraintEqualToConstant(device.frame().width).active = true
-		}
-		
-		if let heightAnchor = height {
-			view.heightAnchor.constraintEqualToConstant(heightAnchor).active = true
-		} else {
-			view.heightAnchor.constraintEqualToConstant(44).active = true
-			print("WARNING: set scrollview height to minimum height: 44")
+			verticalScroller = nil
 		}
 		view.translatesAutoresizingMaskIntoConstraints = false
 
-		super.init()
-		view.delegate = self
+		updateScrollers()
+	}
+	
+	func setupScrollers() {
 		
-		self.drawScrollIndicators()
+		let verticalHeight = viewHeightAnchor! / contentHeight! * viewHeightAnchor! - (2 * scrollerOffset)
+		let horizontalHeight = viewWidthAnchor! / contentWidth! * viewWidthAnchor! - (2 * scrollerOffset)
+		
+		let verticalX = viewWidthAnchor! - scrollerOffset - scrollerThickness
+		let verticalY = 0 + scrollerOffset
+
+		let horizontalX = 0 + scrollerOffset
+		let horizontalY = viewHeightAnchor! - scrollerOffset - scrollerThickness
+		
+		verticalScroller = UIView(frame: CGRectMake(verticalX, verticalY, scrollerThickness, verticalHeight))
+		verticalScroller?.backgroundColor = .blackColor()
+		verticalScroller?.alpha = 0.5
+		verticalScroller?.layer.cornerRadius = 3
+		
+		horizontalScroller = UIView(frame: CGRectMake(horizontalX, horizontalY, horizontalHeight, scrollerThickness))
+		horizontalScroller?.backgroundColor = .blackColor()
+		horizontalScroller?.alpha = 0.5
+		horizontalScroller?.layer.cornerRadius = 3
+
+		view.addSubview(verticalScroller!)
+		view.addSubview(horizontalScroller!)
+		
+		print("setup Scrollers")
+	}
+	
+	func updateScrollers() {
+		if !showScrollers {
+			return
+		}
+		print("updating Scrollers NOW")
+		let contentFrame = view.subviews[0].frame
+
+		if let horizontal = horizontalScroller {
+			print("OLD horizontal frame: \(horizontal.frame)")
+			var scrollerFrame = horizontal.frame
+			let newX = (0 - contentFrame.origin.x) * (viewWidthAnchor! / contentWidth!) + scrollerOffset
+			scrollerFrame.origin.x = newX
+			horizontal.frame = scrollerFrame
+			print("NEW horizontal frame: \(horizontal.frame)")
+		}
+		
+		if let vertical = verticalScroller {
+			print("OLD vertical frame: \(vertical.frame)")
+			var scrollerFrame = vertical.frame
+			let newY = (0 - contentFrame.origin.y) * (viewHeightAnchor! / contentHeight!) + scrollerOffset
+			scrollerFrame.origin.y = newY
+			vertical.frame = scrollerFrame
+			print("NEW vertical frame: \(vertical.frame)")
+		}
+		
 	}
 	
 	public func scrollViewDidScroll(scrollView: UIScrollView) {
@@ -75,13 +165,17 @@ public class ScrollView: NSObject, UIScrollViewDelegate {
 		let half = 1/2 as CGFloat
 		view.subviews[0].center.x = device!.frame().width * half
 		view.subviews[0].center.y = device!.frame().height * half
+		self.updateScrollers()
 	}
 	
 	public func scroll(direction: Direction, amount: CGFloat) {
+	// NB: This method removes the scrollers first since all subviews to the scrollView "view" become reposistioned below.
+		horizontalScroller?.removeFromSuperview()
+		verticalScroller?.removeFromSuperview()
 		let subviews = view.subviews
 		for subview in subviews {
 			var center = subview.center
-			print("old center: \(center)")
+			print("OLD center: \(center)")
 			switch direction {
 			case .Up:
 				center.y -= amount
@@ -93,17 +187,10 @@ public class ScrollView: NSObject, UIScrollViewDelegate {
 				center.x += amount
 			}
 			subview.center = center
-			print("new center: \(center)")
+			print("NEW center: \(center)")
 		}
-	}
-	
-	func drawScrollIndicators() {
-		// TODO: Implement me!
-		
-		// Research iOS behavior first
-		// Make two rounded rects based upon scroller bool properties
-		// Draw lengths to be size of view
-		// Draw position to be postion in content view
+		setupScrollers()
+		updateScrollers()
 	}
 }
 
